962bbfadd621480ad834d22f36f10f0d61257888
Luciano Castro <luciano.castro@stone.com.br>
2024-03-13 13:39:40 -0300

    fix code

--- src/convert_md_2_html.go
+++ src/convert_md_2_html.go
@@ -11,11 +11,23 @@ import (
 
 func convertMarkdownToHTML(input string) (string, error) {
 	output := blackfriday.Run([]byte(input))
-	fmt.Println("HTML: ", output)
+	#fmt.Println("HTML: ", output)
 	return string(output), nil
 }
 
 func processFile(path string, info fs.FileInfo, err error) error {
+	parts := strings.Split(path, "/")
+
+    // Verifica se há pelo menos 3 partes (0-indexed)
+    if len(parts) >= 3 {
+        // Obtém o elemento na posição 2
+        platform := parts[2]
+        product  := parts[3]
+        fmt.Println("Segunda parte do caminho:", platform)
+    } else {
+        fmt.Println("O caminho não contém pelo menos 3 partes.")
+    }
+
 	if strings.HasSuffix(path, ".md") {
 		input, err := ioutil.ReadFile(path)
 		if err != nil {
@@ -27,7 +39,7 @@ func processFile(path string, info fs.FileInfo, err error) error {
 			return err
 		}
 
-		newPath := strings.TrimSuffix(path, ".md") + ".html"
+		newPath := strings.TrimSuffix(path, ".md") + "-" + platform + "-" + product + ".html"
 		err = ioutil.WriteFile(newPath, []byte(htmlContent), info.Mode())
 		if err != nil {
 			return err
